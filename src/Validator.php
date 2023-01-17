8e4031ff88cca8019832be54f93abeb7c80be2fb
Christian Timm <christian@egol.de>
2023-01-17 15:54:47 +0100

    Partials

--- src/Validator.php
+++ src/Validator.php
@@ -14,7 +14,7 @@ class Validator
 
     protected $errors = [];
 
-    public function __construct(SepaContract $sepa, int $schema)
+    public function __construct(SepaContract $sepa, $schema)
     {
         $xml = new DOMDocument();
         $xml->loadXML($sepa, LIBXML_NOBLANKS);
